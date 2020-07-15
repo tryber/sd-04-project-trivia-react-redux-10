4c7026a97496cd2b474100e68677075618247a84
Diego Campos <diegodiko05@gmail.com>
2020-07-15 17:04:14 -0300

    CC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import './App.css';
-import LoginPage from './components/LoginPage';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import './App.css';
 
 export default function App() {
   return (
     <BrowserRouter>
       <div>
-      {/* <div className="App">
+        {/* <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>SUA VEZ</p>
