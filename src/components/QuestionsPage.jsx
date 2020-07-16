b2fd541f2e4294d60473fec811782faed0caafe9
PedroParreiras <44480402+PedroParreiras@users.noreply.github.com>
2020-07-16 14:18:39 -0300

    trocando aspas simples

--- src/components/QuestionsPage.jsx
+++ src/components/QuestionsPage.jsx
@@ -8,21 +8,21 @@ export default class QuestionsPage extends Component {
     return (
       <div>
         {/* <Header /> */}
-        <div className='question-and-answer'>
-          <div className='question-box'>
-            <h2 className='type-of-question'> {TypeOfQuestion}</h2>
-            <h1 className='question-text'>{QuestionText}</h1>
-            <h1 className='timer'>{Timer}</h1>
+        <div className="question-and-answer">
+          <div className="question-box">
+            <h2 className="type-of-question"> {TypeOfQuestion}</h2>
+            <h1 className="question-text">{QuestionText}</h1>
+            <h1 className="timer">{Timer}</h1>
           </div>
-          <div className='answer-box'>
-            <button className='answer' data-testid='btn-answer' type='button'>
+          <div className="answer-box">
+            <button className="answer" data-testid="btn-answer" type="button">
               {Answer}
             </button>
-            <Link to='/ResultsPage'>
+            <Link to="/ResultsPage">
               <button
-                className='button-next'
-                data-testid='btn-next'
-                type='button'>
+                className="button-next"
+                data-testid="btn-next"
+                type="button">
                 "PRÓXIMA"
               </button>
             </Link>
