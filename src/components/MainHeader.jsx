6c474577c1eded7d5a126577062fd880883f7493
PedroParreiras <44480402+PedroParreiras@users.noreply.github.com>
2020-07-17 16:26:45 -0300

    Iniciando a aconstrução do MainHeader

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class MainHeader extends Component {
+  render() {
+    const {username, avatar} = this.props
+    return (
+      <div className="Default-Header">
+          <div className="Avatar-and-Name">
+            <img data-testid="header-profile-picture" className="Avatar-Picture" src={ avatar } alt="User avatar" />
+            <h3 data-testid="header-player-name" className="Name">{ username }</h3>
+          </div>
+          <h3 data-testid="header-score" className="Points" >0</h3>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+    username: state.loginReducer[0].name,
+    avatar: state.gravatarReducer.picture,
+  });
+
+export default connect(mapStateToProps)(MainHeader);
