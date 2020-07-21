cdda64aa730dc7f01afc58e88f78163c14989e86
Thabata <dornelasthabata@gmail.com>
2020-07-20 22:05:17 -0300

    CC

--- src/pages/RankingPage.jsx
+++ src/pages/RankingPage.jsx
@@ -8,7 +8,7 @@ class RankingPage extends React.Component {
 
     return (
       <ul className="ranking-list">
-        {sortUser.map(({ name, score, picture }, index) => {
+        {sortUser.map((name, score, picture, index) => {
           return (
             <li className="list-items">
               {sortUser(picture)}
